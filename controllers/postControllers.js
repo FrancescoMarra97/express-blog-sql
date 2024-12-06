1256fa2639d20472979ac2d5ae5764e1ac4a442c
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-06 15:37:29 +0100

    milestone 2

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -3,6 +3,7 @@ const posts = require('../db/db.js')
 const fs = require('fs')
 const { post } = require('../routes/posts.js')
 const connection = require('../db/connection.js')
+const { error } = require('console')
 
 
 
@@ -71,21 +72,38 @@ const update = (req, res) => {
 }
 
 const destroy = (req, res) => {
-    //find the post by slug
-    const post = posts.find((post) => post.slug === req.params.slug);
-    //check if the user is deleting the correct post
-    if (!post) {
-        return res.status(404).json({ error: "no post found with that slug" })
-    }
-    //remove the post from the posts
-    const newPosts = posts.filter((post) => post.slug !== req.params.slug);
-    //update the file js
-    fs.writeFileSync("./db/db.js", `module.exports = ${JSON.stringify(newPosts, null, 4)}`)
-    //return the updated posts item
-    res.status(200).json({
-        status: 200,
-        data: newPosts,
-        counter: newPosts.length
+    /*    //find the post by slug
+       const post = posts.find((post) => post.slug === req.params.slug);
+       //check if the user is deleting the correct post
+       if (!post) {
+           return res.status(404).json({ error: "no post found with that slug" })
+       }
+       //remove the post from the posts
+       const newPosts = posts.filter((post) => post.slug !== req.params.slug);
+       //update the file js
+       fs.writeFileSync("./db/db.js", `module.exports = ${JSON.stringify(newPosts, null, 4)}`)
+       //return the updated posts item
+       res.status(200).json({
+           status: 200,
+           data: newPosts,
+           counter: newPosts.length
+       }) */
+
+
+    console.log(req.params);
+    //recuperiamo lo id dell'url
+    const id = req.params.id;
+    //eliminaimo il post dal blog
+    const sql = 'DELETE FROM posts WHERE id=?'
+    connection.query(sql, [id], (err, results) => {
+        console.log(err, results);
+        if (err) return res.status(500).json({ error: err })
+
+        if (results.affectedRows === 0) return res.status(404).json({ error: `404! No posts found with this id: ${id}` })
+
+        return res.json({ status: 200, affectedRows: results.affectedRows })
+
+
     })
 }
 module.exports = { show, index, store, update, destroy }
--- routes/posts.js
+++ routes/posts.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const postsControllers = require('../controllers/postControllers.js')
 
-router.get('/:slug', postsControllers.show)
+/* router.get('/:slug', postsControllers.show) */
+router.get('/:id', postsControllers.show)
 router.get('/', postsControllers.index)
 router.post('/', postsControllers.store)
 router.put("/:slug", postsControllers.update)
-router.delete("/:slug", postsControllers.destroy)
+/* router.delete("/:slug", postsControllers.destroy) */
+router.delete("/:id", postsControllers.destroy)
 module.exports = router
